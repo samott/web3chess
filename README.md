f76a34c67918273e72eb889993c85335487f85f0
Shaun Amott <shaun@inerd.com>
2024-04-30 13:24:17 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Web3 Chess [WIP]
+
+## Installation
+
+```bash
+npm install
+```
+
+## Run development server
+
+```bash
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<h1>Web3 Chess</h1>
+		</main>
+	);
+}
